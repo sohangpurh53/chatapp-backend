f200ef2229e21e96737a3d25dbfe7a9b8b57465c
Burhanuddin Sohangpurwala <basohangpur@gmail.com>
2025-11-29 13:47:18 +0530

    added debug log to check fire base v1

--- services/fcmService.js
+++ services/fcmService.js
@@ -45,6 +45,7 @@ class FCMService {
   async sendNotification(userId, notification) {
     try {
       // Check if Firebase messaging is available
+      console.log("tracking one here is messaging....", messaging)
       if (!messaging) {
         console.warn(`⚠️  Firebase not configured - skipping notification for user ${userId}`);
         await Notification.update(
